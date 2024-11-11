2f04fa8b72f306a032838ca60aaa24f125f46a1a
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-11-12 02:53:42 +0900

    feat: 결과 반환 함수 구현

--- number-baseball/number-baseball-JeongYeWon/number.js
+++ number-baseball/number-baseball-JeongYeWon/number.js
@@ -1,10 +1,24 @@
 function createRandomNumbers() {
   let gameNum = [];
   while (gameNum.length < 3) {
-      let num = Math.floor(Math.random() * 10);
-      if (!gameNum.includes(num)) {
-          gameNum.push(num);
-      }
+    let num = Math.floor(Math.random() * 10);
+    if (!gameNum.includes(num)) {
+      gameNum.push(num);
+    }
   }
   return gameNum;
+}
+
+function getResult(randomNumber, userNumbers) {
+  let strikes = 0;
+  let balls = 0;
+
+  for (var i = 0; i < 3; i++) {
+    if (userNumbers[i] === randomNumber[i]) {
+      strikes++;
+    } else if (randomNumber.includes(parseInt(userNumbers[i]))) {
+      balls++;
+    }
+  }
+  return { strikes, balls };
 }
