4c71aad47a2b7b6cc29e452ae1f6e4b37ccaa51e
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-11-12 02:59:56 +0900

    feat: 게임 실행 함수 구현

--- number-baseball/number-baseball-JeongYeWon/number.js
+++ number-baseball/number-baseball-JeongYeWon/number.js
@@ -1,4 +1,4 @@
-function createRandomNumbers() {
+let randomNumbers = () => {
   let gameNum = [];
   while (gameNum.length < 3) {
     let num = Math.floor(Math.random() * 10);
@@ -13,7 +13,7 @@ function getResult(randomNumber, userNumbers) {
   let strikes = 0;
   let balls = 0;
 
-  for (var i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
     if (userNumbers[i] === randomNumber[i]) {
       strikes++;
     } else if (randomNumber.includes(parseInt(userNumbers[i]))) {
@@ -21,4 +21,24 @@ function getResult(randomNumber, userNumbers) {
     }
   }
   return { strikes, balls };
+}
+
+let attempts = 0;
+
+function playGame() {
+  let userGuess = document.getElementById('userGuess').value;
+  if (userGuess.length !== 3 || isNaN(userGuess)) {
+    document.getElementById('result').innerText = '유효한 세자리를 입력하세요.';
+  }
+
+  attempts++;
+  let result = getResult(randomNumbers, userGuess);
+
+  document.getElementById('attempts').innerText = `${attempts}번째 시도 : ${userGuess}`;
+  document.getElementById('result').innerText = `${result.balls}B${result.strikes}S`;
+
+  if (result.strikes === 3) {
+    document.getElementById('result').innerText += `${attempts}번 만에 맞히셨습니다. 게임을 종료합니다.`;
+    document.getElementById('userGuess').disabled = true;
+  }
 }
